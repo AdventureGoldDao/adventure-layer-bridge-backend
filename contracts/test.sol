369b53ac98bc03fffa422839e1a5f8a3614a3545
DavidCoder88 <DavidCoder88@github.com>
2024-05-12 20:59:59 +0800

    update test contract

--- contracts/test.sol
+++ contracts/test.sol
@@ -6,17 +6,26 @@ pragma solidity ^0.8.0;
 contract AdventureLayerTransfer {
     address public immutable owner;
     event FundsReceived(address indexed sender, uint256 amount);
+    event FundsSend(address indexed sender, uint256 amount);
     constructor(){
         owner = msg.sender;
     }
     //deposit
     receive() external payable {
+        //msg.sender.transfer(msg.value);
+        emit FundsReceived(msg.sender, msg.value);
+    }
+    //deposit: transfer money from sender to owner
+    function deposit()  public payable {
+        require(msg.sender.balance >= msg.value, "no enough balance");
+        owner.transfer(msg.value)
+
         emit FundsReceived(msg.sender, msg.value);
     }
     //withdraw
     function withraw(address payable  _to, uint256 amount) external   {
         require(msg.sender == owner, "not owner address")
-        //TODO
         _to.transfer(amount)
+        emit FundsSend(_to, amount);
     }
 }
