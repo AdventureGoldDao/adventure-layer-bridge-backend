99f6be9d93c32876744c9499c55ec028e5eed1ee
DavidCoder88 <david88091@gmail.com>
2024-05-14 23:57:04 +0800

    upload right contract

--- contracts/sepolia.sol
+++ contracts/sepolia.sol
@@ -3,32 +3,26 @@
 pragma solidity ^0.8.0;
 
 
-contract AdventureLayerTransfer {
+contract AdventureLayerSepoliaTransfer {
     address public owner;
-    //mapping(address => uint256) public balances;
 
-    event FundsReceived(address indexed sender, uint256 amount);
-    event FundsSend(address indexed sender, uint256 amount);
+    event Deposit(address indexed sender, uint256 amount, address indexed recipient);
+    event Withdrawal(address indexed recipient, uint256 amount);
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
-
     }
-    //deposit: transfer money from sender to owner
-    function deposit(uint256 amount)  public payable {
-        require(msg.sender.balance >= amount, "no enough balance");
-        
-        payable(owner).transfer(msg.value);
-        emit FundsReceived(msg.sender, amount);
-    }
-    //withdraw：transfer money from owner to target adderss
-    function withraw(address payable _to, uint256 amount)  public payable {
-        require(msg.sender == owner, "not owner address");
-        require(msg.sender.balance >= amount, "no enough balance");
-       
-        _to.transfer(amount);
-        emit FundsSend(_to, amount);
 
+    function deposit(address payable recipient,  uint256 amount) external payable {
+        require(recipient != address(0), "Invalid recipient address");
+        recipient.transfer(amount);
+        emit Deposit(msg.sender, amount, recipient);
+    }
+
+    function withdraw(address payable recipient, uint256 amount) external {
+        require(msg.sender == owner, "Only owner can withdraw");
+        recipient.transfer(amount);
+        emit Withdrawal(recipient, amount);
     }
     function getBalance(address account) public view returns(uint256) {
         return account.balance;
